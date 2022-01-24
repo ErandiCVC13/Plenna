c46168dde1943c42e71300f1010436365538304a
Erandi Carolina Vázquez Cuervo <hgftdrd@hotmail.com>
2022-01-24 11:26:45 -0600

    (NewAppointment): Adding the new appointent page

---
+++ pages/new-appointment/index.js
@@ -0,0 +1,8 @@
+import TextArea from "@src/components/shared/atoms/TextArea";
+import NewAppointmentView from "@src/components/shared/molecule/NewAppointmentView";
+
+const NewAppointment = () => {
+  return <NewAppointmentView />;
+};
+
+export default NewAppointment;
